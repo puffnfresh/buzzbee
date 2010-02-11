e678d690ec846fad3a4bd9fe1857bb3faa92be06
Brian McKenna <brian@brianmckenna.org>
2010-02-12 06:45:21 +1000

    Initial commit

---
+++ buzzbee_site.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+from google.appengine.api import images
+from google.appengine.api import urlfetch
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+import urllib
+
+import os
+import logging
+
+FEED_TEMPLATE = 'http://buzz.googleapis.com/feeds/%s/public/posted'
+
+
+class FeedHandler(webapp.RequestHandler):
+    def get(self, user):
+        url = FEED_TEMPLATE % user
+        result = urlfetch.fetch(url)
+        
+        # IE doesn't seem to work with 'application/atom+xml'
+        self.response.headers['Content-Type'] = 'text/xml; charset=UTF-8'
+
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        self.response.out.write(result.content)
+
+# Used for caching resized feed images.
+class ResizedImage(db.Model):
+    url = db.StringProperty()
+    height = db.IntegerProperty()
+    data = db.BlobProperty()
+
+class ResizeHandler(webapp.RequestHandler):
+    def get(self, h, url):
+        h = int(h)
+
+        query = ResizedImage.all()
+        query.filter('height', h)
+        query.filter('url', url)
+
+        resized_image = query.get()
+        if not resized_image:
+            resized_image = ResizedImage()
+
+            resized_image.url = url
+            resized_image.height = h
+
+            result = urlfetch.fetch(urllib.unquote_plus(url))
+
+            image_data = images.resize(result.content, height=h)
+            resized_image.data = db.Blob(image_data)
+
+            resized_image.put()
+
+        self.response.headers['Content-Type'] = "image/png"
+        self.response.out.write(resized_image.data)
+
+class IndexHandler(webapp.RequestHandler):
+    def get(self):
+        path = os.path.join('templates', 'index.html')
+        content = template.render(path, {})
+        self.response.out.write(content)
+
+class NotFoundHandler(webapp.RequestHandler):
+    def get(self):
+        self.error(404)
+
+        path = os.path.join('templates', 'not_found.html')
+        content = template.render(path, {})
+        self.response.out.write(content)
+
+application = webapp.WSGIApplication(
+    [(r'/feed/(.+)', FeedHandler),
+     (r'/resize/(\d+)/(.+)', ResizeHandler),
+     (r'/', IndexHandler),
+     (r'/.*', NotFoundHandler)])
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
---
+++ static/jquery.buzzbee.js
@@ -0,0 +1,131 @@
+(function($) {
+    $.fn.buzzbee = function(user, options) {
+        options = $.extend({
+            error: 'Unable to connect.',
+            empty: 'No available Buzz.',
+            limit: 10,
+            max_media_height: 120,
+            entry_separator: '',
+            detail_separator: ' - ',
+            url_prefix: 'http://jquery-buzzbee.appspot.com/feed/',
+            url_suffix: ''
+        }, options);
+
+        $.ajax({
+            type: 'GET',
+            dataType: 'xml',
+            url: options.url_prefix + user + options.url_suffix,
+            context: this,
+            error: function(xhr, status) {
+                $(this).text(options.error);
+            },
+            success: function(data) {
+                var buzz_box = this;
+                if($(data).find('entry').length == 0) {
+                    $(buzz_box).text(options.empty);
+                    return;
+                }
+
+                $(buzz_box).text('');
+                var count = 0;
+                $(data).find('entry').each(function() {
+                    count++;
+                    if(options.limit > 0 && count > options.limit) {
+                        return;
+                    }
+
+                    var entry = $('<div></div>')
+                                    .addClass('entry')
+                                    .appendTo(buzz_box);
+
+                    var content = $('<div></div>')
+                                      .addClass('content')
+                                      .appendTo(entry);
+                    content.html($(this).find('> content').text());
+
+                    // Handle media.
+                    $(this).find('> media\\:content').each(function() {
+                        var medium = $(this).attr('medium');
+                        var media = $('<div></div>')
+                                        .addClass('media')
+                                        .addClass(medium)
+                                        .appendTo(content);
+
+                        var link = $('<a></a>')
+                                       .attr('href', $(this).attr('url'))
+                                       .appendTo(media);
+
+                        if(medium == 'image') {
+                            var player = $(this).find('> media\\:player');
+
+                            // See if media needs to be resized.
+                            var src = player.attr('url');
+                            if(options.max_media_height > 0 && player.attr('height') > options.max_media_height) {
+                                src = 'http://jquery-buzzbee.appspot.com/resize/' + options.max_media_height + '/' + escape(src);
+                            }
+
+                            var img = $('<img />')
+                                          .attr('src', src)
+                                          .attr('alt', '')
+                                          .appendTo(link);
+                        }
+                    });
+
+                    var details = $('<div></div>')
+                                      .addClass('details')
+                                      .appendTo(entry);
+
+                    var profile_url = $(this).find('> author uri').text();
+                    var title = $('<a></a>')
+                                    .addClass('title')
+                                    .attr('href', profile_url)
+                                    .appendTo(details);
+                    title.html($(this).find('> title').text());
+
+                    var updated = $('<span></span>')
+                                        .addClass('date')
+                                        .appendTo(details);
+                    var time = $(this).find('> updated').text();
+                    var prettyTime = $.fn.buzzbee.prettyDate(time.replace(/\.\d+/g, ''));
+                    updated.text(prettyTime).attr('title', time);
+
+                    var alternate_link = $(this)
+                                             .find('> link[rel="alternate"]')
+                                             .attr('href');
+
+                    var permalink = $('<a></a>')
+                                        .attr('href', alternate_link)
+                                        .appendTo(details);
+                    var total = $(this).find('> thr\\:total').text();
+                    permalink.text('(' + total + ' comment' + (total != 1 ? 's' : '') + ')');
+
+                    $(details).find('> :gt(0)').before(options.detail_separator);
+                });
+
+                $(buzz_box).find('> :gt(0)').before(options.entry_separator);
+            }
+        });
+
+        return this;
+    };
+
+    // John Resig's "Pretty Date"
+    $.fn.buzzbee.prettyDate = function(time) {
+        var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+            diff = (((new Date()).getTime() - date.getTime()) / 1000),
+            day_diff = Math.floor(diff / 86400);
+
+        if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+            return;
+
+        return day_diff == 0 && (
+                diff < 60 && "just now" ||
+                diff < 120 && "1 minute ago" ||
+                diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+                diff < 7200 && "1 hour ago" ||
+                diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+            day_diff == 1 && "Yesterday" ||
+            day_diff < 7 && day_diff + " days ago" ||
+            day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+    };
+})(jQuery);
